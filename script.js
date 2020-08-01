ce4a5669034ed9aba06df936bc43eabc50f1964d
Braham Prakash <developer.brm@gmail.com>
2020-08-01 19:08:38 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+function fetchItems(gifyURL, old) {
+  if(gifyURL === old) return ;
+  input.classList.add("bg")
+  let imagesURL = []
+
+  fetch(gifyURL)
+  .then(blob => blob.json() )
+  .then(data => {
+    data.data.forEach( (x) => {
+      imagesURL.push(x.images.original.url)
+    } )
+    createImages(imagesURL)
+  })
+  .then(() => input.classList.remove("bg"))
+  .catch( err => console.log(err))
+}
+
+function createImages(urls) {
+  imageContainer.innerHTML = ``
+  urls.forEach( url => {
+    const image = document.createElement("img")
+    image.src = url
+    imageContainer.appendChild(image)
+  } )
+  return
+}
+
+function setURL() {
+  
+  let regex = new RegExp(/[^_\W]+/ig)
+  let word = input.value.match(regex)[0]
+  input.value = word
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=2gfW31dhsPscd6K1Ny1w5c6KLOxOSRvP&q=${word}&limit=${range.valueAsNumber}&offset=0&rating=g&lang=en`
+  fetchItems(url, fetchedURL)
+  fetchedURL = url
+
+}
+
+function changeRange() {
+  rangeDisplay.textContent = range.valueAsNumber
+}
+
+function debounce(func, time) {
+  let t;
+  return function() {
+    if(t) clearTimeout(t)
+    t = setTimeout( () => {
+      func()
+    }, time )
+  }
+}
+
+const imageContainer = document.querySelector(".imagesContainer")
+const input = document.querySelector(`.inputContainer [type="text"]`)
+const range = document.querySelector(`.inputContainer [type="range"]`)
+const rangeDisplay = document.querySelector(".inputContainer .count")
+let fetchedURL = ""
+
+
+input.addEventListener( "input", debounce(setURL, 500) )
+range.addEventListener("input", changeRange)
